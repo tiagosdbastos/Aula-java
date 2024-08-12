a61306947b9d3baffd9eadb030cdeaee76d255d3
tiagosdbastos <tiagosdbastos@gmail.com>
2024-08-12 11:23:21 -0300

    Add files via upload

---
+++ PrimeiroProjeto.java
@@ -0,0 +1,14 @@
+package br.com.aula01;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		double cel = 32;
+		double fah;
+		
+		fah = (cel*1.8) + 32;
+		
+		System.out.println("Sua temperatura em fahreinheit: " + fah);
+	}
+
+}
